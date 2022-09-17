8457213077e1ec1ce6f58d20c857c581ab5c83f8
ViniciusPosssatto <101053966+ViniciusPosssatto@users.noreply.github.com>
2022-09-17 17:06:49 -0300

    FEAT: adicionado controller para games

--- database/src/app/controllers/games.py
+++ database/src/app/controllers/games.py
@@ -1,9 +1,33 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
+
+from src.app.models.game import Game, games_share_schema
+from src.app.utils import exist_game
+from src.app.services.game_service import create_game
 
 
 games = Blueprint('games', __name__, url_prefix="/games")
 
 
-@games.route("/games", methods=['GET'])
+@games.route("/", methods=['GET'])
 def list_all_games():
-    return {}, 200
+
+    query_db_all_games = Game.query.all()
+    dict_db_all_games = games_share_schema.dump(query_db_all_games)
+
+    return (dict_db_all_games), 200
+
+
+@games.route("/create", methods=['POST'])
+def create_games():
+
+    body = request.get_json()
+
+    if exist_game(body["discord"]):
+        return jsonify({"error": "Esse usuário já existe."}), 400
+
+    response = create_game(**body)
+
+    if "error" in response:
+        return jsonify(response), 400
+
+    return jsonify(response), 201
--- database/src/app/utils/__init__.py
+++ database/src/app/utils/__init__.py
@@ -2,7 +2,14 @@ from src.app.models.ad import Ad
 from src.app.models.game import Game
 
 
-def exist_user_discord(body):
-    if Ad.query.filter_by(discord=body).first():
+def exist_user_discord(discord):
+    if Ad.query.filter_by(discord=discord).first():
         return True
     return False
+
+
+def exist_game(name):
+    if Game.query.filter_by(name=name).first():
+        return True
+    return False
+

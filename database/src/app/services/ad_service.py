e1dafb7329b448729d76fd02d17d3629b30c3421
ViniciusPosssatto <101053966+ViniciusPosssatto@users.noreply.github.com>
2022-09-17 17:35:29 -0300

    REFACT: alterado nome de coluna e adicionado password ao Ad

--- database/src/app/services/ad_service.py
+++ database/src/app/services/ad_service.py
@@ -2,12 +2,13 @@ from datetime import date
 from src.app.models.ad import Ad
 
 
-def create_ad(name, hours_playing, discord, week_days, hour_start, hour_end, playing, game_id, create_at = date.today()):
+def create_ad(name, hours_playing, discord, password, week_days, hour_start, hour_end, playing, game_id, create_at = date.today()):
     try:
         Ad.seed(
             name=name, 
             hours_playing=hours_playing, 
-            discord=discord, 
+            discord=discord,
+            password=password,
             week_days=week_days, 
             hour_start=hour_start, 
             hour_end=hour_end,
